901cc40d5668662aafbe19a25e0735a0b831e83f
smilg <jacobasmilg@gmail.com>
2021-04-27 17:58:27 -0400

    fill in stage class

--- swag_stage.py
+++ swag_stage.py
@@ -5,4 +5,6 @@ import pygame
 class SwagStage(pygame.sprite.Sprite):
     def __init__(self, player_1, player_2):
         super().__init__()
-        
+        self.surf = pygame.Surface([1000, 50])
+        self.surf.fill((255,255,255))
+        self.rect = self.image.get_rect(center = (1000/2, 1000-50))
