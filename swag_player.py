7a00a29175031be63f6ccdf8c9b0dd9d70a10f2a
smilg <jacobasmilg@gmail.com>
2021-04-27 17:05:17 -0400

    add sprite inheritance

--- swag_player.py
+++ swag_player.py
@@ -1,9 +1,11 @@
 '''
 docstring moment
 '''
+import pygame
 
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, player_number: int, character: str) -> None:
+        super().__init__()
         self.__player_number = player_number
 
     def attack(self, action):
