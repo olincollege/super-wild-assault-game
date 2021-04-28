345a5e528a277ed30350faa374a52159f132c1fa
Melissa Kazazic <mkazazic01@gmail.com>
2021-04-28 17:11:16 -0400

    fix file importing, add frame repetition

--- swag_animation.py
+++ swag_animation.py
@@ -6,21 +6,33 @@ from os import listdir, path
 from abc import ABC, abstractmethod
 
 class Animation:
+    
+    REPEAT_FRAME = 5
     def __init__(self, character: str, move: str) -> None:
         self.__character_path = path.join('chars', character)
         self.__sprites_path = path.join(self.__character_path, 'sprites', move)
-        self.__sprites_list = [pygame.image.load(frame) for frame in listdir(self.__sprites_path)]
+        sprite_filenames = listdir(self.__sprites_path)
+        sprite_filenames = [path.join(self.__sprites_path, name) for name in sprite_filenames]
+        sprite_filenames.sort()
+        self.__sprites_list = [pygame.image.load(frame) for frame in sprite_filenames]
         self.__framedata_path = path.join(self.__character_path, 'animations', f'{move}.anim')
         self.__move = move
         self.__animation_length = len(self.__sprites_list)
         self.__current_frame = 0
+        self.__repetition = 0
         self.__hurtboxes = []
         self.__hitboxes = []
-        self.__collisions = self.get_collision_boxes()
+        # self.__collisions = self.get_collision_boxes()
+
+    def __repr__(self):
+        return f'{self.__move} frame {self.__current_frame}'
 
     @property
     def move(self) -> str:
-        return __move
+        return self.__move
+
+    def reset(self) -> None:
+        self.__current_frame = 0
 
     def get_collision_boxes(self) -> None:
         framedata = []
@@ -29,10 +41,14 @@ class Animation:
             for row in reader:
                 framedata.append(row)
     
-    def update_frame(self):
-        if self.__current_frame > self.__animation_length:
+    def update_frame(self) -> pygame.Surface:
+        self.__repetition += 1
+        if self.__repetition >= self.REPEAT_FRAME:
+            self.__current_frame += 1
+            self.__repetition = 0
+        if self.__current_frame > self.__animation_length-1:
             self.__current_frame = 0
-
+        return self.__sprites_list[self.__current_frame]
 
 
 class Point2D(NamedTuple):
