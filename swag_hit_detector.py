d1b8b7c0abef69e0a96ecf8c63856bc2aab4bee1
Jacob Smilg <jacobasmilg@gmail.com>
2021-05-07 01:05:12 -0400

    clean up code

--- swag_hit_detector.py
+++ swag_hit_detector.py
@@ -1,13 +1,11 @@
 '''
 Contains the information about game collision between sprites
 '''
-import pygame
 from typing import Tuple
-from swag_stage import SwagStage, SwagBarriers
+from pygame import Vector2
 from swag_player import Player
-from pygame import Rect, Vector2
-from swag_collisionsprite import SwagCollisionSprite
 from swag_helpers import CollisionBox, sign
+
 
 class HitDetector:
     def __init__(self, players: Tuple[Player, Player]):
@@ -16,15 +14,21 @@ class HitDetector:
     def player_collision(self):
         player_collision_boxes = {}
         for player in self.__players:
-            collision_boxes = {'hurt': [CollisionBox(*tuple(box)) for box in player.current_animation.current_hurtboxes],
-                                'hit': [CollisionBox(*tuple(box)) for box in player.current_animation.current_hitboxes]}
+            collision_boxes = {
+                'hurt': [CollisionBox(*tuple(box))
+                         for box in player.current_animation.current_hurtboxes],
+                'hit': [CollisionBox(*tuple(box))
+                        for box in player.current_animation.current_hitboxes]}
             for box_type in collision_boxes:
                 for index in range(len(collision_boxes[box_type])):
-                    collision_rect = collision_boxes[box_type][index].rect.move(player.rect.x, player.rect.y)
-                    if player._facing_left:
+                    collision_rect = collision_boxes[box_type][index].rect.move(
+                        player.rect.x, player.rect.y)
+                    if player.facing_left:
                         collision_rect.move_ip(
-                            -2*abs(collision_rect.right-player.pos.x)+collision_rect.w,0)
-                    collision_boxes[box_type][index] = collision_boxes[box_type][index]._replace(rect=collision_rect)
+                            -2*abs(collision_rect.right-player.pos.x)+collision_rect.w, 0)
+                    collision_boxes[box_type][index] = \
+                        collision_boxes[box_type][index]._replace(
+                            rect=collision_rect)
             player_collision_boxes[player.player_number] = collision_boxes
 
         for hurt_player in self.__players:
@@ -32,12 +36,15 @@ class HitDetector:
                 hit_player_num = 1
                 if hurt_player.player_number == 1:
                     hit_player_num = 2
-                
-                hurt_rect = hurtbox.rect
 
-                collision = hurt_rect.collidelist([hitbox.rect for hitbox in player_collision_boxes[hit_player_num]['hit']])
+                hurt_rect = hurtbox.rect
+                collision = hurt_rect.collidelist(
+                    [hitbox.rect for hitbox in player_collision_boxes[hit_player_num]['hit']])
                 if collision != -1:
                     collision_box = player_collision_boxes[hit_player_num]['hit'][collision]
-                    self.__players[hurt_player.player_number-1].attacked(collision_box.damage,
+                    self.__players[hurt_player.player_number-1].attacked(
+                        collision_box.damage,
                         collision_box.knockback_scale,
-                        Vector2(sign(-1*self.__players[hit_player_num-1]._facing_left)*abs(collision_box.knockback_x), collision_box.knockback_y))
+                        Vector2(sign(-1*self.__players[hit_player_num-1].facing_left)
+                                * abs(collision_box.knockback_x),
+                                collision_box.knockback_y))
