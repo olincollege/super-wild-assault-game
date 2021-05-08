d69a0789bc317cc530aa3aa3b1840602879fd784
Jacob Smilg <jacobasmilg@gmail.com>
2021-05-07 21:43:55 -0400

    clean up code

--- swag_game.py
+++ swag_game.py
@@ -2,12 +2,11 @@
 Swag Game 👍
 '''
 
-from swag_stage import SwagStage, SwagStageBackground, SwagBarriers
 import sys
-import time
 import pygame
-from pygame.locals import *     # type: ignore  pylint: disable=wildcard-import
+from pygame.locals import QUIT
 from swag_player import Player
+from swag_stage import SwagStage, SwagStageBackground, SwagBarriers
 from swag_view import PygameView
 from swag_input_handler import PygameInput
 from swag_hit_detector import HitDetector
@@ -27,26 +26,31 @@ if __name__ == '__main__':
     P1 = Player(1, 'olinman', STAGE, BARRIER_SPRITES)
     P2 = Player(2, 'catboy', STAGE, BARRIER_SPRITES)
 
-    # create view
+
     VIEW = PygameView(BACKGROUND,STAGE,LEFT_BARRIER,RIGHT_BARRIER, P1, P2)
     HIT_DETECTOR = HitDetector((P1, P2))
-
     CONTROLLERS = [PygameInput(P1), PygameInput(P2)]
-    game_end = False
+
+    # used to close the game when someone wins
+    GAME_END = False
 
     VIEW.draw()
+
+    # game loop 
     while True:
         for event in pygame.event.get():
-            # Will run when the close window button is clicked    
-            if event.type == QUIT or game_end == True:
+            # Will run when the close window button is clicked
+            if event.type == QUIT or GAME_END:
                 pygame.quit()
                 sys.exit()
         if not P1.lost and not P2.lost:
             for controller in CONTROLLERS:
                 controller.poll_input()
-        elif not game_end:
-            print(f'{P2.character_name * P1.lost}{P1.character_name * P2.lost} wins! Restart the game to play again.')
-            game_end = True
+        elif not GAME_END:
+            print(f'{P2.character_name * P1.lost}{P1.character_name * P2.lost}' + \
+                    ' wins! Restart the game to play again.')
+            GAME_END = True
+
         # check collisions then update p1 and p2
         HIT_DETECTOR.player_collision()
         P1.update()
